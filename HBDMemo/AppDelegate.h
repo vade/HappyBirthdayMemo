dbe7cbd27f2f2e60f6193068c565bded12167494
vade <vade@vade.info>
2014-08-28 14:17:30 -0400

    Initial Commit

---
+++ HBDMemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HBDMemo
+//
+//  Created by vade on 8/28/14.
+//  Copyright (c) 2014 vade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HBDMemo/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  HBDMemo
+//
+
+//  Copyright (c) 2014 vade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
